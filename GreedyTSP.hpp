a4820a02887ca3a0a9a8ab0afd1d154921c1bf7b
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2023-12-22 22:26:06 -0500

    Add files via upload

--- GreedyTSP.hpp
+++ GreedyTSP.hpp
@@ -1,5 +1,5 @@
-#ifndef NEARESTNEIGHBOR_HPP
-#define NEARESTNEIGHBOR_HPP
+#ifndef GREEDYTSP_HPP
+#define GREEDYTSP_HPP
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -7,45 +7,49 @@
 #include <algorithm>
 #include <ctime>
 #include <cmath>
+#include <limits>
 #include "Node.hpp"
-class NearestNeighbor {
+
+class GreedyTSP {
 public:
-    static void nearestNeighbor(const std::string& filename) {
-        //PROCESS TO READ FILE
+    static void greedyTSP(const std::string& filename) {
         std::ifstream file(filename);
         if (!file.is_open()) {
-            std::cerr << "Ekror " << filename << std::endl;
+            std::cerr << "Error opening file: " << filename << std::endl;
             return;
         }
+
         std::string line;
         std::vector<Node> nodes;
-        bool startReading = false; 
+        bool startReading = false;
+
         while (std::getline(file, line)) {
-            //LAST LINE
             if (line.find("NODE_COORD_SECTION") != std::string::npos) {
                 startReading = true;
                 break;
             }
         }
+
         while (std::getline(file, line)) {
             if (isdigit(line[0])) {
                 int id;
                 double x, y;
                 std::istringstream iss(line);
                 iss >> id >> x >> y;
-                nodes.push_back(Node(id, x, y)); 
+                nodes.push_back(Node(id, x, y));
             }
         }
 
-        // BEGIN ALGO
         std::vector<int> visitedNodes;
         visitedNodes.push_back(1);
         double totalDistance = 0.0;
         auto start = std::clock();
+
         while (visitedNodes.size() < nodes.size()) {
             int current = visitedNodes.back();
             double minimumDistance = std::numeric_limits<double>::max();
             int nearestNode = -1;
+
             for (const auto& node : nodes) {
                 if (std::find(visitedNodes.begin(), visitedNodes.end(), node.id) == visitedNodes.end()) {
                     double dist = nodes[current - 1].distance(node);
@@ -55,24 +59,31 @@ public:
                     }
                 }
             }
+
             if (nearestNode != -1) {
                 visitedNodes.push_back(nearestNode);
             } else {
-                std::cerr << "cant find nearest node" << std::endl;
+                std::cerr << "Can't find nearest node." << std::endl;
                 break;
             }
+
             totalDistance += minimumDistance;
         }
+
         visitedNodes.push_back(1);
         totalDistance += nodes[visitedNodes.back() - 1].distance(nodes[0]);
+
         auto endTime = std::clock();
         double totalTime = static_cast<double>(endTime - start) / CLOCKS_PER_SEC * 1000.0;
+
         std::cout << "Path: ";
         for (int node : visitedNodes) {
             std::cout << node << " ";
         }
+
         std::cout << "\nTotal Distance: " << totalDistance << "\nTime in ms: " << totalTime << std::endl;
         file.close();
     }
 };
-#endif
+
+#endif 
---
+++ Node.hpp
@@ -0,0 +1,22 @@
+// Node.hpp
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include <cmath>
+
+class Node {
+public:
+    int id;
+    double x, y;
+    Node* edge1;  // Represents the first edge
+    Node* edge2;  // Represents the second edge
+
+    Node(int _id, double _x, double _y) : id(_id), x(_x), y(_y), edge1(nullptr), edge2(nullptr) {}
+
+    // Method to calculate distance between two nodes
+    double distance(const Node& other) const {
+        return std::sqrt(std::pow(x - other.x, 2) + std::pow(y - other.y, 2));
+    }
+};
+
+#endif // NODE_HPP
